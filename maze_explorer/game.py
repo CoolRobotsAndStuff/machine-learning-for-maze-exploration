bbdba868682df16588584fb29dec3f9e3c76a5e3
CoolRobotsAndStuff <aledeum.saf@gmail.com>
2022-03-30 12:02:53 -0300

    agregados comentarios a game.py

--- maze_explorer/game.py
+++ maze_explorer/game.py
@@ -14,75 +14,63 @@ import utils
 """
 class Maze_Game():
     def __init__(self, grid:list, detection_distance:int, initial_position:list=[0,0], initial_orientation:str="up"):
-
-        self.entire_grid = copy.deepcopy(grid)  # El mundo
-        
+        # Distance from the center of the robot inside wich the robot will detect things
         self.detection_distance = detection_distance
-        self.grid_shape = [len(self.entire_grid), len(self.entire_grid[0])]
-
-
-        self.dicovered_grid = self.create_discovered(self.entire_grid)
-
+        
         self.directions = {"up":[-1,0], "down":[1,0], "right":[0,1], "left":[0,-1]}
-
-        self.robot_position = initial_position
-        self.robot_orientation = initial_orientation
-
-        self.reacheable = self.get_reachable_nodes()
-        self.explored = set()
-        self.update_explored()
-
         self.time_to_move = 1
         self.time_to_turn = 1
 
-        self.total_time = 0
+        self.reset_game(grid, initial_position, initial_orientation)
 
-        self.updateMask()
-    
+    # Resets the game
     def reset_game(self, grid:list, initial_position:list=[0,0], initial_orientation:str="up"):
-        self.entire_grid = copy.deepcopy(grid)
+        self.entire_grid = copy.deepcopy(grid)  # The world
+        self.grid_shape = [len(self.entire_grid), len(self.entire_grid[0])]  # The shape of the world
+        # The part of the world the robot has discovered so far
         self.dicovered_grid = self.create_discovered(self.entire_grid)
         self.robot_position = initial_position
         self.robot_orientation = initial_orientation
-        self.updateMask()
-        self.reacheable = self.get_reachable_nodes()
-        self.explored = set()
-        self.update_explored()
-        self.total_time = 0
+        self.updateMask()  # Makes the robot discover the world aorund it
+        self.reacheable = self.get_reachable_nodes()  # The tiles the robot is able to explore in the world
+        self.explored = set() # The tiles the robot has explored in the world
+        self.update_explored() # Add the tiles around it
+        self.total_time = 0 # Total time in the run
 
         return self.dicovered_grid
     
+    # Returns the nodes that are reachable from the robot
     def get_reachable_nodes(self):
-        # Create a queue to store nodes
-        queue = []
-        # Add the starting node
-        queue.append(tuple(self.robot_position))
-        # Create a set to store visited nodes
-        visited = set()
-
-        visited_tiles = set()
+        queue = []  # Create a queue to store nodes
+        queue.append(tuple(self.robot_position))  # Add the starting node
+        visited = set()  # Create a set to store visited verticies
+        visited_tiles = set() # Create a set to store visited tiles around visited verticies
         # While queue is not empty
         while queue:
             # Dequeue a node from queue and add it to visited set
             current_node = queue.pop(0)
             visited.add(current_node)
 
+            # Add the adjacent tiles of the node to the visited_tiles set
             for adj in utils.get_adjacents(current_node, include_straight=False, include_diagonals=True):
                 if adj not in visited_tiles:
                     visited_tiles.add(adj)
 
+            # Get the valid ajacent nodes
             adjacent_nodes = []
             for key, val in self.directions.items():
                 adajcent = (current_node[0] + val[0]*2, current_node[1] + val[1]*2)
-                if self.is_valid_move(key) and self.is_valid_position(adajcent):
+                if self.is_valid_move(current_node, key):
                     adjacent_nodes.append(adajcent)
+
             # If any adjacent node is not visited, enqueue it
             for node in adjacent_nodes:
                 if node not in visited and node not in queue:
                     queue.append(node)
         return visited_tiles
 
-
+    # Creates an empty grid for the robot to gradually discover
+    # Only leaves the node_type, wich is already known
     def create_discovered(self, grid):
         disc_grid = []
         for row in grid:
@@ -115,73 +103,70 @@ class Maze_Game():
         min_y = max(self.robot_position[1] - self.detection_distance, 0)
         max_y = min(self.robot_position[1] + self.detection_distance + 1, self.grid_shape[1])
         
-        # descubrir de la mascara todos indices dentro del cuadrado 
-        #falso = ya descubierto 
+        # añadir al mapa descubierto los nodos dentro del cuadrado
         for x in range(min_x, max_x):
             for y in range(min_y, max_y):
                 self.dicovered_grid[x][y].status = self.entire_grid[x][y].status
                 self.dicovered_grid[x][y].tile_type = self.entire_grid[x][y].tile_type
 
-    def is_valid_move(self, move:str):
+    # Is the position in bounds of the grid
+    def is_in_bounds(self, position):
+        is_valid = True
+        for pos, shape in zip(position, self.grid_shape[0:2]):
+            if pos < 0 or pos > shape -1:
+                is_valid = False
+        return is_valid
+
+    # Is a position valid?
+    def is_valid_position(self, position):
+        is_valid = self.is_in_bounds(position)
+        # Is the position not occupied
+        if is_valid:
+            is_valid = self.entire_grid[position[0]][position[1]].status != "occupied"
+        
+        # Are the walls surrounding the position not occupied
+        for adjacent in utils.get_adjacents(position):
+            if self.entire_grid[adjacent[0]][adjacent[1]].status == "occupied":
+                is_valid = False
+
+        return is_valid
+
+    # Given the initial position and the direction of the movement, is it valid?
+    def is_valid_move(self, init_pos:list, move:str):
         if move not in self.directions.keys():
             return False
-        new_pos = [(pos1 + pos2*2) for pos1, pos2 in zip(self.robot_position, self.directions[move])]
-        middle_pos = [(pos1 + pos2) for pos1, pos2 in zip(self.robot_position, self.directions[move])]
+        new_pos = [(pos1 + pos2*2) for pos1, pos2 in zip(init_pos, self.directions[move])]
+        middle_pos = [(pos1 + pos2) for pos1, pos2 in zip(init_pos, self.directions[move])]
         if self.is_valid_position(new_pos) and self.is_valid_position(middle_pos):
             return True
         else:
             return False
 
-    # Executes a movement
+    # Executes a movement. Returns if the movement was valid and the time taken to do it.
     def move(self, move:str):
-        if self.is_valid_move(move):
+        if self.is_valid_move(self.robot_position, move):
             new_pos = [(pos1 + pos2*2) for pos1, pos2 in zip(self.robot_position, self.directions[move])]
-        
             # Change the robot position
             self.robot_position = new_pos
-
             # Number of turns to get to new orientation to be able to move in that direction
             n_turns = self.get_change_in_orientation(self.robot_orientation, move)
-
             # Set the orientation of the robot
             self.robot_orientation = move
-
+            # Time taken to execute the movement
             time_taken = self.time_to_move + (self.time_to_turn * n_turns)
 
             return True, time_taken
         else:
             return False, 0
     
-    def is_in_bounds(self, position):
-        is_valid = True
-        # Is the position in bounds of the grid
-        for pos, shape in zip(position, self.grid_shape[0:2]):
-            if pos < 0 or pos > shape -1:
-                is_valid = False
-        return is_valid
-    
-    # Is a position valid?
-    def is_valid_position(self, position):
-        is_valid = self.is_in_bounds(position)
-
-        # Is the position not occupied
-        if is_valid:
-            is_valid = self.entire_grid[position[0]][position[1]].status != "occupied"
-        
-        if self.entire_grid[position[0]][position[1]].node_type == "vortex":
-            for adjacent in utils.get_adjacents(position):
-                if self.is_in_bounds(adjacent):
-                    if self.entire_grid[adjacent[0]][adjacent[1]].status == "occupied":
-                        is_valid = False
-
-        return is_valid
-    
+    # If the robot has explored the entire grid
     def finished(self):
         #print("reacheable", self.reacheable)
         #print("explored", self.explored)
         #print("left", self.reacheable - self.explored)
         return self.reacheable == self.explored
     
+    # Updates the part of the map the robot can see depending on its position
     def update_explored(self):
         for adj in utils.get_adjacents(self.robot_position, include_straight=False, include_diagonals=True):
             if adj not in self.explored:
@@ -228,47 +213,14 @@ class Maze_Game():
                 elif y == robot_right[1] and x == robot_down[0]:
                     print("◤ ", end="")
                 
-                    
                 else:
                     print(str(value), end="")
             print("\n", end="")
-
         print("")
 
 
 if __name__ == "__main__":
-
-    """
-    # Esto crea un escenario de ejemplo
-
-    grid = make_grid((17, 17))
-
-    for value in grid[-1]:
-        value.status = "occupied"
-    
-    for value in grid[0]:
-        value.status = "occupied"
-    
-    for value in grid[12][6:16]:
-        value.status = "occupied"
-    
-    for index, row in enumerate(grid):
-        row[-1].status = "occupied"
-        row[0].status = "occupied"
-        if index in range(4, 9):
-            row[8].status = "occupied"
-    
-    grid[1][1].tile_type = "start"
-    grid[1][3].tile_type = "start"
-    grid[3][1].tile_type = "start"
-    grid[3][3].tile_type = "start"
-
-    grid[-2][-2].tile_type = "hole"
-    grid[-2][-4].tile_type = "hole"
-    grid[-4][-2].tile_type = "hole"
-    grid[-4][-4].tile_type = "hole"
-
-    """
+    # Loads a map
     script_dir = os.path.dirname(__file__)
     rel_path = "3by3.json"
     abs_file_path = os.path.join(script_dir, rel_path)
@@ -285,15 +237,17 @@ if __name__ == "__main__":
     print('---------------------------------')
     input('Ingresar cualquier caracter para continuar: ')
     maze.print_grid()
+
     while True:
-            
             move = input("move: ")
             if move == "exit": break
+
             valid_move, _, time_taken = maze.step(move)
             if not valid_move:
                 print("invalid movement!")
             print("Time taken:", time_taken)
             maze.print_grid()
+
             if maze.finished():
                 print("Finished!")
                 break
