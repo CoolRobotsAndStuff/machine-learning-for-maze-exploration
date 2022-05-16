633371f228474407b3ccfcd0302147560a272043
CoolRobotsAndStuff <aledeum.saf@gmail.com>
2022-05-16 11:15:51 -0300

    yes flags in apt installs

--- install_ubuntu.py
+++ install_ubuntu.py
@@ -6,9 +6,9 @@ parent_dir = path.parent.absolute().parent.absolute()
 
 subprocess.run(["apt-get", "update"])
 
-subprocess.run(["apt-get", "install", "python3-pip"])
+subprocess.run(["apt-get", "install", "python3-pip", "-y"])
 
-subprocess.run(["apt-get", "install", "git"])
+subprocess.run(["apt-get", "install", "git", "-y"])
 
 subprocess.run(["pip", "install", "numpy", "gym", "tensorflow", "keras", "mlflow", "bresenham", "google-cloud-storage"])
 
